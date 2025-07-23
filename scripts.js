16e605e98961b66081337a76917e4142da38924d
ApkBonney <adombonney1@gmail.com>
2025-07-23 10:19:26 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -20,3 +20,20 @@ alert("Thank you for your interest! We will conact you shortly with our plan det
 // Reset form
 this.reset()
 })
+
+// show popup after 5 seconds
+setTimeout(function(){
+    document.getElementById("email-popup").style.display="flex"
+},5000);
+
+// close popup when X is clicked
+document.querySelector(".close-btn").addEventListener("click",function(){
+    document.getElementById("email-popup").style.display="none";
+})
+
+// close when clicking ouside content
+document.getElementById("email-popup").addEventListener("click",function(e){
+    if(e.target=== this){
+        this.style.display="none";
+    }
+})
